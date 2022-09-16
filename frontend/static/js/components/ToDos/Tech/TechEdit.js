a15c300b1416e8ab83d1049eea664cf67f1f9499
KsaweryKapela <ksawerykapela97@gmail.com>
2022-09-16 18:53:26 +0200

    Deleted some redundant files from git.

--- frontend/static/js/components/ToDos/Tech/TechEdit.js
+++ frontend/static/js/components/ToDos/Tech/TechEdit.js
@@ -12,38 +12,47 @@ export default function TechEdit(tech) {
 
     const handleInputChange = (event) => setTechName(event.target.value)
     const handleKeypress = (event) => {
-    if (event.keyCode === 13) {
+        if (event.keyCode === 13) {
 
-      axios({
-             method: "patch",
-             url: "http://127.0.0.1:8000/edit-tech/",
-             data: {name: techName,
-                    old_name: oldName},
-             headers: {
-                       'Accept': 'application/json',
-                       'Content-Type': 'application/json',
-                       'X-CSRFToken': csrftoken
-                       }})
+            axios({
+                method: "patch",
+                url: "http://127.0.0.1:8000/edit-tech/",
+                data: {
+                    name: techName,
+                    old_name: oldName
+                },
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'X-CSRFToken': csrftoken
+                }
+            })
 
-        if (techName.trim() === ''){
-            return window.location.reload()}
+            if (techName.trim() === '') {
+                return window.location.reload()
+            }
 
-        setIsClicked(!isClicked)
-    }}
+            setIsClicked(!isClicked)
+        }
+    }
 
     return (
         <>
             {!isClicked &&
-            <h1 onClick={() => setIsClicked(!isClicked)}>{techName}</h1>}
+                <h1 onClick={() => setIsClicked(!isClicked)}>{techName}</h1>}
 
             {isClicked &&
 
-             <input className='tech-edit-input'
-                    onChange={handleInputChange}
-                    onKeyDown={handleKeypress}
-                    value={techName}
-                    onFocus={(e) => {formOnClick(e); setOldName(e.target.value)}}
-                    autoFocus
-                    onMouseLeave={() => setIsClicked(!isClicked)}
-             /> }</>
-    )}
+                <input className='tech-edit-input'
+                       onChange={handleInputChange}
+                       onKeyDown={handleKeypress}
+                       value={techName}
+                       onFocus={(e) => {
+                           formOnClick(e);
+                           setOldName(e.target.value)
+                       }}
+                       autoFocus
+                       onMouseLeave={() => setIsClicked(!isClicked)}
+                />}</>
+    )
+}
